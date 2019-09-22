22ff6cdd5f24cf8e2d6852b1cd6e6111066312cd
Lucian Condrea <luciancondreadev@gmail.com>
2019-09-22 17:22:00 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-title">
+        June Health
+      </header>
+    </div>
+  );
+}
+
+export default App;
