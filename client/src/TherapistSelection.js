dde5f7ff898bfdf0238a3e9751417face3f6a9ea
Lucian Condrea <luciancondreadev@gmail.com>
2019-09-22 18:04:24 -0400

    add therapist avatar

--- client/src/TherapistSelection.js
+++ client/src/TherapistSelection.js
@@ -1,30 +1,35 @@
 import React from 'react';
+import av1 from './avatars/trp1.jpg';
+import av2 from './avatars/trp2.jpg';
+import av3 from './avatars/trp3.jpg';
+import av4 from './avatars/trp4.jpg';
+import av5 from './avatars/trp5.jpg';
 
 const THERAPIST_LIST = [
   {
     name: 'Nicholas Doe',
     specialization: 'Psychologist',
-    avatar: ''
+    avatar: av1
   },
   {
     name: 'John Doe',
     specialization: 'Cognitive Psychology',
-    avatar: ''
+    avatar: av2
   },
   {
     name: 'Jessica Doe',
     specialization: 'Counseling Psychology',
-    avatar: ''
+    avatar: av3
   },
   {
     name: 'Anthony Doe',
     specialization: 'Couples Counseling',
-    avatar: ''
+    avatar: av4
   },
   {
     name: 'Samantha Doe',
     specialization: 'Clinical Psychology',
-    avatar: ''
+    avatar: av5
   },
 ]
 
@@ -34,7 +39,7 @@ function TherapistSelection() {
       <p>Select your Therapist:</p>
       { THERAPIST_LIST.map(therapist => (
         <div>
-          <img src={therapist.avatar} alt='therapist portrait' />
+          <img className='therapist-avatar' src={therapist.avatar} alt='therapist portrait' />
           <h2>{therapist.name}</h2>
           <p>{therapist.specialization}</p>
         </div>
