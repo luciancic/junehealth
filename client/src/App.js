2e975c4b59c825cb0c207c992cd7775ea7256bb9
Lucian Condrea <luciancondreadev@gmail.com>
2019-09-24 12:36:49 -0400

    refactor title

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
+import Title from './components/Title';
 import TherapistSelection from './TherapistSelection';
 import './App.css';
-import titleImg from './assets/title.png';
 
 function App() {
   return (
     <div className="app">
-      <header className="app-title">
-        <img src={ titleImg } alt="title"/>
-      </header>
+      <Title />
       <TherapistSelection />
     </div>
   );
---
+++ client/src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import titleImg from '../assets/title.png';
+
+function Title() {
+  return (
+    <header className="app-title">
+      <img src={ titleImg } alt="title"/>
+    </header>
+  )
+}
+
+export default Title;
