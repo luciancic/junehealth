1091bdd38f56d2527aecd40902493b4e1f1e98ae
Lucian Condrea <luciancondreadev@gmail.com>
2019-09-28 19:59:43 -0400

    setup proxy for react app

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.get('*', (req,res) =>{
 	res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Started up server'))
