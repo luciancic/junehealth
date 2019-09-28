062b47810a0cb9dd51caa7a31c2e77c0555f0a8f
Lucian Condrea <luciancondreadev@gmail.com>
2019-09-28 19:45:03 -0400

    insert therapists script

---
+++ init/insertTherapists.js
@@ -0,0 +1,33 @@
+const Therapist = require('../db/Therapist')
+
+const THERAPIST_LIST = [
+  {
+    name: 'Nicholas Doe',
+    specialization: 'Psychologist',
+    avatarFile: 'av1.jpg'
+  },
+  {
+    name: 'John Doe',
+    specialization: 'Cognitive Psychology',
+    avatarFile: 'av2.jpg'
+  },
+  {
+    name: 'Jessica Doe',
+    specialization: 'Counseling Psychology',
+    avatarFile: 'av3.jpg'
+  },
+  {
+    name: 'Anthony Doe',
+    specialization: 'Couples Counseling',
+    avatarFile: 'av4.jpg'
+  },
+  {
+    name: 'Samantha Doe',
+    specialization: 'Clinical Psychology',
+    avatarFile: 'av5.jpg'
+  },
+]
+
+Therapist.sync().then(() => {
+  THERAPIST_LIST.forEach(th => Therapist.create(th))
+})
