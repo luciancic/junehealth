c58438de549b0ab3b82cd2b721c8c7bde45132fd
Lucian Condrea <luciancondreadev@gmail.com>
2019-09-28 20:13:07 -0400

    get therapists from server

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,22 @@ import Splash from './containers/Splash';
 import TherapistSelection from './containers/TherapistSelection';
 import Wait from './containers/Wait';
 
-import therapists from './data/therapists';
-
 import './App.css';
 
 function App() {
   // Values can be 'splash', 'select', 'wait'
   const [page, setPage] = useState('splash');
+  const [therapists, setTherapists] = useState(null);
 
   useEffect(() => {
     // Simulate loading of client. Will change later
     setTimeout(() => setPage('select'), 1000)
+    fetch('/api/therapists').then(res => res.json()).then(data => setTherapists(data))
   }, [])
   
   return (
     <div className="app">
-      { page === 'splash' ? <Splash /> :
+      { !therapists || (page === 'splash') ? <Splash /> :
         page === 'select' ? <TherapistSelection data={therapists} handleClick={() => setPage('wait')} /> :
         page === 'wait' ? <Wait close={() => setPage('select')} /> : null }
     </div>
