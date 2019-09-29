021dfcfa7b1ace08d846b88d75469eaaf4f04035
Lucian Condrea <luciancondreadev@gmail.com>
2019-09-28 20:04:38 -0400

    get avatars from server

--- client/src/data/therapists.js
+++ client/src/data/therapists.js
@@ -1,34 +1,34 @@
-import av1 from './avatars/trp1.jpg';
-import av2 from './avatars/trp2.jpg';
-import av3 from './avatars/trp3.jpg';
-import av4 from './avatars/trp4.jpg';
-import av5 from './avatars/trp5.jpg';
+// import av1 from './avatars/trp1.jpg';
+// import av2 from './avatars/trp2.jpg';
+// import av3 from './avatars/trp3.jpg';
+// import av4 from './avatars/trp4.jpg';
+// import av5 from './avatars/trp5.jpg';
 
 const THERAPIST_LIST = [
   {
     name: 'Nicholas Doe',
     specialization: 'Psychologist',
-    avatar: av1
+    avatar: 'av1.jpg'
   },
   {
     name: 'John Doe',
     specialization: 'Cognitive Psychology',
-    avatar: av2
+    avatar: 'av2.jpg'
   },
   {
     name: 'Jessica Doe',
     specialization: 'Counseling Psychology',
-    avatar: av3
+    avatar: 'av3.jpg'
   },
   {
     name: 'Anthony Doe',
     specialization: 'Couples Counseling',
-    avatar: av4
+    avatar: 'av4.jpg'
   },
   {
     name: 'Samantha Doe',
     specialization: 'Clinical Psychology',
-    avatar: av5
+    avatar: 'av5.jpg'
   },
 ]
 
